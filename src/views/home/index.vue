076000aab1606026fdc4da1b1cbd42e384e3fb40
Jiao Jian <903939365@qq.com>
2019-12-18 21:59:23 +0800

    验证（一半）

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -2,16 +2,16 @@
 <div class="box">
 <el-card class="form">
     <div class="tu"><img src="../../assets/logo_index.png" alt=""></div>
-    <el-form>
-        <el-form-item>
-            <el-input placeholder="请输入手机号"></el-input>
+    <el-form :model="shuxing" :rules="guize">
+        <el-form-item prop="mobile">
+            <el-input v-model="shuxing.mobile" placeholder="请输入手机号"></el-input>
         </el-form-item>
-        <el-form-item>
-            <el-input style="width:65%" placeholder="请输入验证码"></el-input>
+        <el-form-item prop="verify">
+            <el-input v-model="shuxing.verify" style="width:65%" placeholder="请输入验证码"></el-input>
             <el-button style="float:right">发送验证码</el-button>
         </el-form-item>
-        <el-form-item>
-            <el-checkbox>你必须同意做他女朋友</el-checkbox>
+        <el-form-item prop="checkbox">
+            <el-checkbox v-model="shuxing.checkbox">你必须同意做他女朋友</el-checkbox>
         </el-form-item>
         <el-form-item>
             <el-button style="width:100%" type="primary">登录</el-button>
@@ -24,11 +24,26 @@
 
 <script>
 export default {
+  data () {
+    return {
+      shuxing: {
+        mobile: '',
+        verify: '',
+        checkbox: false
 
+      },
+      guize: {
+        mobile: [{ required: true, message: '请输入手机号' },
+          { pattern: /^(13[0-9]|14[5|7]|15[0|1|2|3|4|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/, message: '手机号输入错误' }],
+        verify: [{ required: true, message: '请输入验证码' },
+          { pattern: /^\d{6}$/, message: '验证码输入错误' }]
+      }
+    }
+  }
 }
 </script>
 
-<style lang="less">
+<style lang="less" scoped>
 .box{
     /* width: 100%; */
     background: url("../../assets/bgtu2.jpg");
