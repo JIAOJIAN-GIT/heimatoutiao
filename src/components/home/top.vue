2d669f305d9e65cca64dc3f2c93a5368e8da926b
Jiao Jian <903939365@qq.com>
2019-12-22 09:33:22 +0800

    黑马头条PC-主页模块-头部结构及下拉菜单

---
+++ src/components/home/top.vue
@@ -0,0 +1,45 @@
+<template>
+    <el-row class="top">
+        <el-col :span="12">
+            <i class="el-icon-s-fold"></i>
+            <span>江苏黑马xxxxxxx</span>
+        </el-col>
+        <el-col class="you" :span="12">
+            <el-row  type="flex" justify="end">
+                <img src="../../assets/bgtu.jpg" alt="">
+                <el-dropdown>
+                    <span>大仙娱乐</span>
+                    <el-dropdown-menu slot="dropdown">
+                        <el-dropdown-item>个人信息</el-dropdown-item>
+                        <el-dropdown-item>GIT信息</el-dropdown-item>
+                        <el-dropdown-item>退出</el-dropdown-item>
+                    </el-dropdown-menu>
+                </el-dropdown>
+            </el-row>
+        </el-col>
+    </el-row>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scopet>
+.top{
+height: 50px;
+line-height: 50px;
+i{
+    margin-right: 5px;
+}
+.you{
+    img{
+        width: 40px;
+        height: 40px;
+        border-radius: 50%;
+        margin: 5px;
+    }
+}
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,23 @@
 <template>
 <el-container>
-  <el-aside>left</el-aside>
+  <el-aside style="height:100vh;background-color:#353b4e;width:230px">
+    <leftlist></leftlist>
+  </el-aside>
   <el-container>
-    <el-header>top</el-header>
+    <el-header><top></top></el-header>
     <el-min>content</el-min>
   </el-container>
 </el-container>
 </template>
 
 <script>
+import leftlist from '../../components/home/left-list.vue'
+import top from '../../components/home/top.vue'
 export default {
+  components: {
+    'leftlist': leftlist,
+    'top': top
+  }
 }
 </script>
 
