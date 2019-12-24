bc5d4d76d38de5cb9c9258a19645a574a493644c
Jiao Jian <903939365@qq.com>
2019-12-24 10:54:30 +0800

    面包屑

---
+++ src/components/commen/bread-crunmd.vue
@@ -0,0 +1,18 @@
+<template>
+    <el-breadcrumb>
+        <el-breadctunmb-item>首页</el-breadctunmb-item>
+        <el-breadctunmb-item>
+            <slot name="title"></slot>
+        </el-breadctunmb-item>
+    </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 
 import leftlist from './home/left-list.vue'
 import top from './home/top.vue'
+import breadrunmb from './commen/bread-crunmd.vue'
 
 export default {
   install (Vue) {
     Vue.component('leftlist', leftlist)
     Vue.component('top', top)
+    Vue.component('bread-crunmb', breadrunmb)
   }
 }
