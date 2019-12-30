dd6529fdade1f9a7f151d1b6d896e978a32bd817
Jiao Jian <903939365@qq.com>
2019-12-30 09:55:53 +0800

    素材列表-页面结构-tab

--- src/views/comment/material.vue
+++ src/views/comment/material.vue
@@ -1,11 +1,22 @@
 <template>
-<div>素材管理
-</div>
+    <el-card>
+        <bread-crunmb slot="header">
+        <template slot="title">素材管理</template>
+        </bread-crunmb>
+        <el-tabs type="card" v-model="tabs">
+            <el-tab-pane label="全部" name="all">全部</el-tab-pane>
+            <el-tab-pane label="收藏" name="collect">收藏</el-tab-pane>
+        </el-tabs>
+    </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tabs: 'all'
+    }
+  }
 }
 </script>
 
