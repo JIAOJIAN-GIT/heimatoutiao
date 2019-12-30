57c7cc48e511f4a49e366b5bb39d42aeee8befb5
Jiao Jian <903939365@qq.com>
2019-12-30 11:48:23 +0800

    素材列表-数据加载

--- src/views/comment/material.vue
+++ src/views/comment/material.vue
@@ -4,8 +4,24 @@
         <template slot="title">素材管理</template>
         </bread-crunmb>
         <el-tabs type="card" v-model="tabs">
-            <el-tab-pane label="全部" name="all">全部</el-tab-pane>
-            <el-tab-pane label="收藏" name="collect">收藏</el-tab-pane>
+            <el-tab-pane label="全部" name="all">
+                <el-row type="flex"  style="flex-wrap:wrap" class="row">
+                    <el-card class="card" v-for="item in list" :key="item.id">
+                        <img :src="item.url?item.url:tuUrl" alt="">
+                        <el-row class="icon" type="flex" align="middle" justify="space-around">
+                            <i class="el-icon-star-on" style="font-size:20px"></i>
+                            <i class="el-icon-delete-solid"></i>
+                        </el-row>
+                    </el-card>
+                </el-row>
+            </el-tab-pane>
+            <el-tab-pane label="收藏" name="collect">
+                <el-row type="flex"  style="flex-wrap:wrap" class="row">
+                    <el-card class="card" v-for="item in list" :key="item.id">
+                        <img :src="item.url?item.url:tuUrl" alt="">
+                    </el-card>
+                </el-row>
+            </el-tab-pane>
         </el-tabs>
     </el-card>
 </template>
@@ -14,12 +30,49 @@
 export default {
   data () {
     return {
-      tabs: 'all'
+      tabs: 'all',
+      list: [],
+      tuUrl: require('../../assets/404.png')
     }
+  },
+  methods: {
+    getall () {
+      this.$axios({
+        url: '/user/images'
+      }).then(res => {
+        console.log(res.data.results)
+        this.list = res.data.results
+      })
+    }
+  },
+  created () {
+    this.getall()
   }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.row{
+    .card{
+        margin: 20px 50px;
+        width: 210px;
+        height: 190px;
+        padding: 0px;
+        position: relative;
+        img {
+            width: 100%;
+            height: 100%;
+        }
+        .icon{
+            position: absolute;
+            left: 0px;
+            bottom: 0px;
+            height: 40px;
+            font-size: 17px;
+            width: 100%;
+            margin: 0px;
+            background: #f4f5f6;
+        }
+    }
+}
 </style>
